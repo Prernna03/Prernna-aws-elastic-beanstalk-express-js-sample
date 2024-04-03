be74cc9b5991d2027cb36009d3c6c88045bc39ca
Prernna03 <164213275+Prernna03@users.noreply.github.com>
2024-04-03 12:46:23 +0530

    hii added

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello lakshana and amirtha!'));
+
+app.get('/', (req, res) => res.send('Hello lakshana and amirtha! hiii'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
