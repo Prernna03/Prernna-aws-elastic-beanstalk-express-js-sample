cc0ecce4b5c6dd2842f0bfdfe011075455113dc2
Prernna03 <164213275+Prernna03@users.noreply.github.com>
2024-04-03 11:54:09 +0530

    name change

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Prernna!'));
+app.get('/', (req, res) => res.send('Hello lakshana and amirtha!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
